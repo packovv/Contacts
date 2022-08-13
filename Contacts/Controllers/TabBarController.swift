41891d1f27dd454a81cb7af98aa1a9852c500ad8
Pavel Kachan <KachanPG@yandex.ru>
2022-08-13 22:09:05 +0300

    TabBarController is done Need to create ContactsViewCell

--- Contacts/Controllers/TabBarController.swift
+++ Contacts/Controllers/TabBarController.swift
@@ -11,11 +11,28 @@ class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        UITabBar.appearance().barTintColor = .systemBackground
+        tabBar.tintColor = .label
+        setupVCs()
     }
     
+    fileprivate func createNavigaionController(for rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.tabBarItem.title = title
+        navigationController.tabBarItem.image = image
+        navigationController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        return navigationController
+    }
 
+    func setupVCs() {
+        viewControllers = [
+            createNavigaionController(for: ContactsViewController(), title: NSLocalizedString("Contacts", comment: ""), image: UIImage(systemName: "person.fill")!),
+            createNavigaionController(for: PersonsViewController(), title: NSLocalizedString("Persons", comment: ""), image: UIImage(systemName: "magazine.fill")!)
+        ]
+    }
+    
     /*
     // MARK: - Navigation
 
